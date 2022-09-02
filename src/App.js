92dbf4cde00058f1e8ca70c697c861d07289eb5c
Giuseppe Caponetto <giuseppe.caponetto@geoimpact.ch>
2022-09-02 14:43:07 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import Phaser from "phaser/dist/phaser-arcade-physics.min"
+import {useEffect} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+      function preload() {
+        this.load.setBaseURL('https://labs.phaser.io');
+
+        this.load.image('sky', 'assets/skies/space3.png');
+        this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+        this.load.image('red', 'assets/particles/red.png');
+      }
+      function create() {
+        this.add.image(400, 300, 'sky');
+
+        var particles = this.add.particles('red');
+
+        var emitter = particles.createEmitter({
+          speed: 100,
+          scale: {start: 1, end: 0},
+          blendMode: 'ADD'
+        });
+
+        var logo = this.physics.add.image(400, 100, 'logo');
+
+        logo.setVelocity(100, 200);
+        logo.setBounce(1, 1);
+        logo.setCollideWorldBounds(true);
+
+        emitter.startFollow(logo);
+      }
+        let config = {
+            type: Phaser.AUTO,
+            width: 600,
+            height: 500,
+            physics: {
+                default: 'arcade',
+                arcade: {
+                    gravity: {y: 200}
+                }
+            },
+            scene: {
+                preload: preload,
+                create: create
+            }
+        };
+
+        let game = new Phaser.Game(config);
+        return () => {
+            game.destroy(true, false);
+        }
+    }, [])
+    return (
+        <div className="App">
+
+        </div>
+    );
 }
 
 export default App;
